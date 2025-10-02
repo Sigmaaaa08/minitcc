a4c0882613dc02d02d64b9b96b194daf98877b0a
Lucas Gailhardo <lucasdepaulagailhardo@gmail.com>
2025-10-02 15:49:44 -0300

    Adição de Model: Conexao.class

---
+++ MiniTCCEstacionamento/src/Model/Conexao.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author LABINFO
+ */
+public class Conexao {
+    Connection conn = null;
+
+    public Connection conectar() {
+        String url = "jdbc:mysql://localhost/BDESTACIONAMENTO";
+        String usuario = "root";
+        String senha = "";
+
+        try {
+            //Iniciando um novo caminho entre a aplicação e o banco
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //passando as informações do banco
+            //Instanciando o objeto do tipo Connection
+            conn = DriverManager.getConnection(url, usuario, senha);
+            System.out.println("Conexão criada com sucesso!!");
+            return conn;
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            return null;
+        } catch (Exception e) {
+            System.out.println("Erro ao conectar: " + e);
+            return null;
+        }
+    }
+
+    public void desconectar() {
+        try {
+            conn.close();
+            System.out.println("Banco Sem Conexão");
+        } catch (SQLException ex) {
+            System.out.println("Erro ao desconectar: " + ex);
+        }
+    }
+}
